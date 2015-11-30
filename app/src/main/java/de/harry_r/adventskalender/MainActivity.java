83cf3b5fc39d2073b14f3e46d265182151287e01
Leonard Rapp <leonardrapp@gmx.de>
2015-11-30 23:45:17 +0100

    hide status and action bar, added background

--- app/src/main/java/de/harry_r/adventskalender/MainActivity.java
+++ app/src/main/java/de/harry_r/adventskalender/MainActivity.java
@@ -2,12 +2,18 @@ package de.harry_r.adventskalender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         setContentView(R.layout.activity_main);
     }
 }
