a22df72d67a4224fcbebdf8af5e75b6856c441a6
Raphael Lehmann <raphael@rleh.de>
2015-11-30 22:04:32 +0100

    removed 'Hello World', added ACRA crash reporting

---
+++ app/src/main/java/de/harry_r/adventskalender/MyApplication.java
@@ -0,0 +1,29 @@
+package de.harry_r.adventskalender;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+@ReportsCrashes(
+        httpMethod = HttpSender.Method.PUT,
+        reportType = HttpSender.Type.JSON,
+        formUri = "http://acra.mytfg.de/acra-adventskalender/_design/acra-storage/_update/report",
+        formUriBasicAuthLogin = "adventskalender_app",
+        formUriBasicAuthPassword = "1hv8AeshKfgWyKtbsJxM420La8iLcRUlEg==",
+
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast
+)
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialise ACRA:
+        ACRA.init(this);
+    }
+}
