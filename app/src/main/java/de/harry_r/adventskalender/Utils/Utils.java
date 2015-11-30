bc18456ce29fb96cbf01fc5ac76dda2848d2e76d
Raphael Lehmann <raphael@rleh.de>
2015-11-30 23:59:00 +0100

    Utils class: added showComment...() methods; comment strings

--- app/src/main/java/de/harry_r/adventskalender/Utils/Utils.java
+++ app/src/main/java/de/harry_r/adventskalender/Utils/Utils.java
@@ -1,6 +1,13 @@
 package de.harry_r.adventskalender.Utils;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
 import java.util.Calendar;
+import java.util.Random;
+
+import de.harry_r.adventskalender.R;
 
 public class Utils {
     /**
@@ -17,5 +24,63 @@ public class Utils {
         } else {
             return 0;
         }
+    }
+
+    /**
+     * Creates a dialog with random comments (door opened too late)
+     * @param context context for dialog
+     */
+    public static void showCommentTooLate(Context context) {
+        // get string array from resources
+        String[] comments = context.getResources().getStringArray(R.array.comments_too_late);
+
+        // select one string from array using a random generated index
+        Random random = new Random();
+        int random_index = random.nextInt(comments.length);
+        String comment = comments[random_index];
+
+        // special comment on monday
+        Calendar cal = Calendar.getInstance();
+        if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+            if (random_index > (comments.length/2)) {
+                comment = context.getResources().getString(R.string.comment_too_late_monday);
+            }
+        }
+
+        // create and show alert dialog
+        new AlertDialog.Builder(context)
+                .setTitle("Zu spät")
+                .setMessage(comment)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Creates a dialog with random comments (door opened too early)
+     * @param context context for dialog
+     */
+    public static void showCommentTooEarly(Context context) {
+        // get string array from resources
+        String[] comments = context.getResources().getStringArray(R.array.comments_too_early);
+
+        // select one string from array using a random generated index
+        Random random = new Random();
+        int random_index = random.nextInt(comments.length);
+        String comment = comments[random_index];
+
+        // create and show alert dialog
+        new AlertDialog.Builder(context)
+                .setTitle("Zu früh")
+                .setMessage(comment)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .show();
     }
 }
