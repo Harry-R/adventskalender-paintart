485d01eaefcd91e6ad07f0da44169e9ff9b48b7c
Leonard Rapp <leonardrapp@gmx.de>
2015-12-01 01:06:11 +0100

    added door buttons and onclick functions

--- app/src/main/java/de/harry_r/adventskalender/MainActivity.java
+++ app/src/main/java/de/harry_r/adventskalender/MainActivity.java
@@ -3,6 +3,10 @@ package de.harry_r.adventskalender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import de.harry_r.adventskalender.Utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
+
+    private void doDoorAction(int number) {
+        switch (Utils.checkDate(number)) {
+            case 0:
+                Utils.showCommentTooLate(this);
+                break;
+            case 1:
+                // show image
+                break;
+            case 2:
+                Utils.showCommentTooEarly(this);
+                break;
+
+        }
+    }
+
+
+    public void open_door(View view) {
+        Button door = (Button) view;
+        int number = Integer.parseInt(door.getText().toString());
+        doDoorAction(number);
+    }
+
 }
--- app/src/main/java/de/harry_r/adventskalender/Utils/Utils.java
+++ app/src/main/java/de/harry_r/adventskalender/Utils/Utils.java
@@ -3,6 +3,8 @@ package de.harry_r.adventskalender.Utils;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Random;
